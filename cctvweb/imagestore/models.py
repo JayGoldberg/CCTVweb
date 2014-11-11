a8838cf8d9e72e8f6b2654679d6e5a3f03004f08
Jay Goldberg <jaymgoldberg@gmail.com>
2014-11-10 18:50:19 -0600

    nicely built models and updated DB

--- cctvweb/imagestore/models.py
+++ cctvweb/imagestore/models.py
@@ -2,35 +2,38 @@ from django.db import models
 
 # Camera details
 class Camera(models.Model):
-    name = models.CharField(max_length=200)  
+    name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    mac = models.CharField(max_length=17)
+    mac = models.CharField(max_length=17,unique=True)
     ip = models.GenericIPAddressField()
     hostname = models.CharField(max_length=200)
-    thumbnail = models.CharField(max_length=200) 
-#    resolution = 
-#    downsample =
-#    sunrise = 
-#    sunset =
-#    caption1 =
-#    caption2 =
-#    prebuffer = 
-#    prebuffer_delay = 
-#    postbuffer =
-#    postbuffer_delay =
+    stream_url = models.CharField(max_length=100,default='now.jpg&push=spush0.5')
+    snapshot_url = models.CharField(max_length=100,default='now.jpg')
+    username = models.CharField(max_length=32,default='root')
+    password = models.CharField(max_length=32,default='system')
+    thumbnail = models.CharField(max_length=255)
+    add_date = models.DateTimeField(auto_now_add=True)
+#    resolutionx = models.PositiveSmallIntegerField()
+#    resolutiony = models.PositiveSmallIntegerField()
+#    downsample =  models.PositiveSmallIntegerField()
+#    sunrise = models.DateTimeField()
+#    sunset = models.DateTimeField()
+#    caption1 = CharField(max_length=50)
+#    caption2 = CharField(max_length=50)
+#    prebuffer = models.PositiveSmallIntegerField()
+#    prebuffer_delay = models.PositiveSmallIntegerField()
+#    postbuffer = models.PositiveSmallIntegerField()
+#    postbuffer_delay = models.PositiveSmallIntegerField()
 #    timezone = 
-#    long = 
-#    lat = 
-    enabled = models.BooleanField()
-#    live_monitoring = models.BooleanField()
-    add_date = models.DateTimeField('date added')
+#    long = models.DecimalField(max_digits=3,decimal_places=6)
+#    lat = models.DecimalField(max_digits=3,decimal_places=6)
+    enabled = models.BooleanField(default=False)
 
 #class Event(models.Model):
-#    image_id = foreign key to Image table
+#    image_id = models.ForeignKey('Image')
 #    datetime = models.DateTimeField('event date and time')
-#    trigger_type = models.CharField(max_length=200) # how this event was triggered
-#    sequence = models.smallint(default=0)
-#    event_id = primary key for this table
+#    trigger_type = models.CommaSeparatedIntegerField(max_length=9)
+#    sequence = models.PositiveSmallIntegerField()
 #    archive_delete = # when to delete this event (and the associated images)
 #    archive_until = models.DateTimeField('event date and time')
 #    thumbnail = 
@@ -38,14 +41,40 @@ class Camera(models.Model):
 class Image(models.Model):
     path = models.CharField(max_length=255)
     raw_json = models.CharField(max_length=255)
-    mac = models.CharField(max_length=255)
-    timestamp = models.DateTimeField
-    trigger_type = models
-    sequence_number = models.IntegerField()
+    # Wanted to use MAC, but I feel like it should be pointing to primary key
+    # because you could take a camera and move it, and want to restart it in a different
+    # location while keeping the old image and event data
+    mac = models.ForeignKey('Camera')
+    timestamp = models.DateTimeField()
+    trigger_type = models.CommaSeparatedIntegerField(max_length=9) # trigger that was active at time
+    sequence_number = models.PositiveSmallIntegerField()
     event_id = models.IntegerField()
-    cam_id = models.ForeignKey(Camera)
-    deleted = models.BooleanField()
-    thumbnail_event =  
+    is_archived = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    # by default, we will use the middle frame in the sequence of images in 
+    thumbnail = models.CharField(max_length=255)
+
+# define tags
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    # I guess we can do this, so that delete of a tag can be reverted
+    is_deleted = models.BooleanField(default=False)
+    
+# supports tag-to-image relationships
+class ImageTag(models.Model):
+    # images can have many tags, and many tags can be attached to many images
+    image_id = models.ManyToManyField('Image')
+    tag_id = models.ForeignKey('Tag')
+    # not sure if this is required
+    #image_is_deleted = models.ForeignKey('Image',to_field='is_deleted')
+    #tag_is_deleted = models.ForeignKey('Tag',to_field='is_deleted')
+
+# Notify certain users depending on camera/events
+#class Subscriptions(model.Models):
+#    user_id = models.ForeignKey(User)
+#    cam_id = models.ForeignKey(Camera)
+#    window_id = window of camera where detection will send notification of motion to this user
 
 # Probably should not do user mgmt here, use Django's native user system
 #class User(model.Models):
@@ -57,20 +86,3 @@ class Image(models.Model):
 
 #class UserRole():
 #
-
-    
-# define tags
-class Tag(model.Models):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    
-# supports tagging events
-#class EventTags(model.Models):
-#    event_id = models.ManyToManyField(Event)
-#    tag_id = models.ForeignKey(Tag)
-
-# Notify certain users depending on camera/events
-#class Subscriptions(model.Models):
-#    user_id = models.ForeignKey(User)
-#    cam_id = models.ForeignKey(Camera)
-#    window_id = window of camera where detection will send notification of motion to this user
