a0b0a3ef4603e488985d05ed1c6cc014e2cfc3d7
Jay Goldberg <jaymgoldberg@gmail.com>
2014-12-20 16:36:58 -0600

    first revision of import script

---
+++ cctvweb/imagestore/csvimport.py
@@ -0,0 +1,35 @@
+from django.db import models
+from imagestore.models import Image
+import os, sys
+import csv
+import json
+from pprint import pprint
+import datetime
+
+print("Started")
+
+f = open('../sanitized.csv')
+reader = csv.reader(f)
+
+for row in reader:
+    # parse the json
+    decoded = json.loads(row[1])
+    
+    #pprint(decoded)
+    
+    #if decoded['IQimage']['event'][0] == 'motion':
+    #    print(decoded['IQimage']['motionWindows'])
+    print(decoded['IQimage']['time'])
+   
+    imageFrame = Image()
+    imageFrame.path = row[0]
+    imageFrame.raw_json = row[1]
+    #imageFrame.mac = 
+    # datetime expects a decimal, not an extended UNIX time string
+    imageFrame.timestamp = datetime.datetime.utcfromtimestamp(decoded['IQimage']['time']/1000.0)
+    imageFrame.imgjdbg = decoded['IQimage']['imgjdbg']
+    imageFrame.sequence_number = decoded['IQimage']['sequence']
+    imageFrame.trigger_type = decoded['IQimage']['event'] # this is not ideal as it will store an array
+    imageFrame.save()
+
+f.close()
