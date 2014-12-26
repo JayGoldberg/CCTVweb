1e1806ffaf08bce331abcfa54dbfe88f7edec855
Jay Goldberg <jaymgoldberg@gmail.com>
2014-12-26 17:49:56 -0600

    test for existence of motionWindows field, otherwise you get a keyerror on an line of CSV that does not contain it

--- cctvweb/imagestore/csvimport.py
+++ cctvweb/imagestore/csvimport.py
@@ -7,16 +7,22 @@ import datetime
 
 print("Started")
 
-f = open('../data/sanitized.csv')
+f = open(os.path.join('..', 'data', 'sanitized.csv'))
 reader = csv.reader(f)
 
 for row in reader:
-    # parse the json
+    # parse the json field in the CSV
     decoded = json.loads(row[1])
     
+    # this test does not add appreciably to the insert time
+    if decoded['IQimage']['event'] == ['motion']:
+        insert_motion_windows = decoded['IQimage']['motionWindows']
+    else:
+        insert_motion_windows = ''
+    
     # datetime expects a decimal, not an extended UNIX time string, thus /1000.0
-    # trigger_type below is not ideal as it will store an array == bad db normalization
-    imageFrame = Image(path = row[0], raw_json = row[1], timestamp = datetime.datetime.utcfromtimestamp(decoded['IQimage']['time']/1000.0), imgjdbg = decoded['IQimage']['imgjdbg'], sequence_number = decoded['IQimage']['sequence'], trigger_type = decoded['IQimage']['event'])
+    # trigger_type & motion_windows are not ideal as they are stored as arraya == bad db normalization
+    imageFrame = Image(path = row[0], raw_json = row[1], timestamp = datetime.datetime.utcfromtimestamp(decoded['IQimage']['time']/1000.0), imgjdbg = decoded['IQimage']['imgjdbg'],  sequence_number = decoded['IQimage']['sequence'], trigger_type = decoded['IQimage']['event'],  motion_windows = insert_motion_windows)
     
     imageFrame.save()
 
