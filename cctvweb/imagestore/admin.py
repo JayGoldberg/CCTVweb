ba0f4da860841230335af437a1fb213bc871d011
Jay Goldberg <jaymgoldberg@gmail.com>
2015-02-10 09:27:40 -0600

    remove old code

--- cctvweb/imagestore/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from imagestore.models import Camera,Image,Tag
-
-# Register your models here.
-admin.site.register(Camera)
-admin.site.register(Image)
-admin.site.register(Tag)
