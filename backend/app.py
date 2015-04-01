6b9126870082bda0c6581cad78b1ad25425b9e69
Jay Goldberg <jaymgoldberg@gmail.com>
2015-03-31 22:53:48 -0500

    unify results: output on return, add frame counter on each event, and return this value.

--- backend/app.py
+++ backend/app.py
@@ -44,41 +44,44 @@ class Events(Resource):
     #result = collection.find({ '$and': [ { "IQimage.time" : { '$gt': int(epoch_start), '$lt': int(epoch_end) } }, {'path': {'$regex': regex} } ] }, { '_id': 0, 'IQimage.imgjdbg': 0, 'IQimage.sequence': 0 } ).sort("IQimage.time")
     result = collection.find({ "IQimage.time" : { '$gt': start_datetime, '$lt': end_datetime } }, { '_id': 0, 'IQimage.imgjdbg': 0, 'IQimage.sequence': 0 } ).sort("IQimage.time")
     
-    return {'request data': request.args, 'est_size': '%sMB' % round(((result.count() * 300)/1024),2), 'start_date': start_datetime, 'end_date': end_datetime, 'results': list(result), 'resultcount': result.count()}, 200
+    return { 'args': request.args, 'result': list(result[0:200]), 'est_size': '%sMB' % round(((result.count() * 300)/1024),2), 'start_date': start_datetime, 'end_date': end_datetime, 'resultcount': result.count() }, 200
 
   def post(self, start_datetime, end_datetime):
     parser = reqparse.RequestParser()
-    parser.add_argument('group', type=bool, help='group the events requires true or false', default=True)
-    parser.add_argument('dwell_time_secs', type=int, help='time before the next activity is considered a new event', default=3)
-    parser.add_argument('sloppy_results', type=bool, help='if the time window "cuts off" the event, those extra JPG frames are included anyway', default=False)
+    parser.add_argument('group', type=bool, help='group the events requires true or false', location='json', default=True)
+    parser.add_argument('dwell_time_secs', type=int, help='time before the next activity is considered a new event', location='json', default=3)
+    parser.add_argument('sloppy_results', type=bool, help='if the time window "cuts off" the event, those extra JPG frames are included anyway', location='json', default=False)
     
     args = parser.parse_args()
+    print(args)
     
     result = collection.find({ "IQimage.time" : { '$gt': start_datetime, '$lt': end_datetime } }, { '_id': 0, 'IQimage.imgjdbg': 0, 'IQimage.sequence': 0 } ).sort("IQimage.time")
 
     if args['group'] == True and result.count() != 0:
       event_list = []
-      results = list(result) # can only use this once, it empties the result object
+      results = list(result[0:10000]) # can only use this once, it empties the result object, REMOVE this limit after testing
       dwell_time = int(args['dwell_time_secs']) * 1000
 
       startFrame = results[0]['IQimage']['time']
       lastFrame = results[0]['IQimage']['time']
+      
+      frameCount = 0
       for image in results:
-        print(image['IQimage']['time'])
+        frameCount += 1
         difference = image['IQimage']['time'] - lastFrame
         
         if difference > dwell_time:
           # this is a new event
-          event_list.append([startFrame, lastFrame])
+          event_list.append([startFrame, lastFrame, frameCount])
           startFrame = image['IQimage']['time']
+          frameCount = 0
         
         lastFrame = image['IQimage']['time']
-        
         # BUG: last if there is no end to the last event, those JPGs are not included in the grouping
     
-      return { 'args': args, 'resultcount': len(event_list), 'event_grouping': event_list}, 200
+      return { 'args': request.args, 'result': event_list, 'resultcount': len(event_list) }, 200
     else:
-      return {'request data': request.args, 'est_size': '%sMB' % round(((result.count() * 300)/1024),2), 'start_date': start_datetime, 'end_date': end_datetime, 'results': list(result), 'resultcount': result.count()}, 200
+      return { 'args': request.args, 'result': list(result[0:4000]), 'est_size': '%sMB' % round(((result.count() * 300)/1024),2), 'start_date': start_datetime, 'end_date': end_datetime, 'resultcount': result.count() }, 200
     
   def delete(self):
     return {'request data': request.args}
