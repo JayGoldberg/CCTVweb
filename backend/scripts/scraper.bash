bb90fc0eebd8f4fc49626321da8e4457436bf0b7
root <root@imagestore.(none)>
2015-04-08 23:21:37 -0500

    change to use rdjpgcom, about 4x faster. 29m24.209s to scrape 1227183 images

--- backend/scripts/scraper.bash
+++ backend/scripts/scraper.bash
@@ -3,11 +3,14 @@
 #title         :scraper.bash
 #description   :This script scans a directory for JPEGs and saves their comment data to a CSV
 #author        :Jay Goldberg
-#date          :20141101
+#date          :20150401
 #license       :MIT
-#version       :0.0.1
-#usage         :bash scraper.bash <dir with hour dirs> <filename.csv>
+#version       :0.0.2
+#usage         :bash scraper.bash <dir with day dirs> <filename.csv>
 #notes         :make sure that column heading are stripped on each subsequent run of exiftool in the loop
 #==============================================================================
 
-for hour in "$1"/??; do exiftool -r -a -G4 -csv -Comment "$hour" >> $2; done
+for day in `ls ${1}`; do for hour in `ls ${1}/${day}`; do for file in `ls ${day}/${hour}`; do echo -n ${day}/${hour}/${file}, >> ${2}; rdjpgcom ${day}/${hour}/${file} | sed -n -e '2{p;q}' >> ${2}; done; done; done
+
+# this method is actually slower! thought that one file write per loop would be faster...
+#for day in `ls ${1}`; do for hour in `ls ${1}/${day}`; do for file in `ls ${day}/${hour}`; do filepath="${day}/${hour}/${file},"; comment=$(rdjpgcom ${day}/${hour}/${file} | sed -n -e '2{p;q}'); echo ${filepath}{$comment} >> ${2}; done; done; done
