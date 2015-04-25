bcb6fe9b822b294a426d29aa721772ed19b24cf3
Jay Goldberg <jaymgoldberg@gmail.com>
2015-04-25 14:01:03 -0500

    oops, committed wrong version, this one's got those changes

--- backend/scripts/csvimport.py
+++ backend/scripts/csvimport.py
@@ -4,21 +4,63 @@ __author__ = "Jay Goldberg"
 __copyright__ = "Copyright 2015"
 __credits__ = ["Jay Goldberg"]
 __license__ = "MIT"
+__version__ = "0.0.1"
 __maintainer__ = "Jay Goldberg"
 __email__ = "jaymgoldberg@gmail.com"
+__status__ = "Alpha"
 
 import os, sys, csv, json, datetime
 import pymongo
 from pymongo import MongoClient
 
+class Importer:
+  def __init__(self, numlines_per_chunk):
+    self.chunksize = numlines_per_chunk
+    self.iterator = 0
+    self.chunk = []
+    self.progress = 0
+      
+  def addline(self, line):
+    if self.iterator < self.chunksize:
+      self.chunk.append(line)
+      self.iterator += 1
+    else:
+      # we are missing files at the end
+      print("Commit!")
+      self.mongocommit()
+      self.chunk.append(line)
+      self.iterator += 1
+
+  def mongocommit(self):
+    #print('Beginning insert')
+    bulk_data = []
+    for i in self.chunk:
+      pair = i.split(',', 1)
+      path = pair[0] # the first element is always the path
+      decoded = json.loads(pair[1]) # parse the json field in the CSV
+      new_record = {"path": path,
+              "IQimage": decoded['IQimage']}
+      bulk_data.append(new_record)
+
+    collection.insert(bulk_data)
+    #print('Finished insert')
+    self.iterator = 0
+    self.chunk = []
+    self.progress += 1
+    print(self.progress * self.chunksize)
+      
+  def complete(self):
+    collection.create_index("IQimage.time", pymongo.ASCENDING)
+
 filepath = sys.argv[1]
+
+mongochunker = Importer(5000)
 
 if os.path.exists(filepath):
   print('Opening file %s' % filepath)
 
   # TODO: put this in a try block
   f = open(filepath)
-  #reader = csv.reader(f, quotechar='{', quoting=csv.QUOTE_MINIMAL)
 
   # TODO: define DB settings in config file
   print('Connecting to DB')
@@ -26,43 +68,17 @@ if os.path.exists(filepath):
   db = client.mydb
   collection = db.images
   print('Connected to DB')
+  print('Drop collection')
   
-  print('Building insert data')
-  bulk_data = []
-  #for row in reader:
-  #    print(row[1])
-  #    path = row[0] # the first element is always the path
-  #    decoded = json.loads(row[1]) # parse the json field in the CSV
-  #    
-  #    new_record = {"path": path,
-  #           "IQimage": decoded['IQimage']}
-  #    bulk_data.append(new_record)
-  
-  records = []
-  for line in f:
-      records.append(line.split(',', 1))
-
-  for pair in records:
-      path = pair[0] # the first element is always the path
-      decoded = json.loads(pair[1]) # parse the json field in the CSV
-
-      new_record = {"path": path,
-           "IQimage": decoded['IQimage']}
-      bulk_data.append(new_record)
-      
   collection.drop()
   
-  print('Beginning insert')
-  for i in range(0, len(bulk_data), 2000):
-      collection.insert(bulk_data[i:i+2000])
-      print(i) # progress indicator
-  print('Finished insert')
+  print('Read input data')
   
+  for line in f:
+    mongochunker.addline(line)
   f.close()
-
-  # this doesn't seem to work right now. Run manually in mongo with 'db.images.createIndex( { "IQimage.time": 1 } )'
+  
   print("Building index")
-  collection.create_index("IQimage.time", pymongo.ASCENDING)
   
 else:
   print('File does not exist')
