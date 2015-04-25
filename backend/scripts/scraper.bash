aee49e2c80605dd722cde68d796caea497f73674
Jay Goldberg <jaymgoldberg@gmail.com>
2015-04-25 13:51:32 -0500

    more readable, remove some header metadata

--- backend/scripts/scraper.bash
+++ backend/scripts/scraper.bash
@@ -3,14 +3,22 @@
 #title         :scraper.bash
 #description   :This script scans a directory for JPEGs and saves their comment data to a CSV
 #author        :Jay Goldberg
-#date          :20150401
 #license       :MIT
-#version       :0.0.2
 #usage         :bash scraper.bash <dir with day dirs> <filename.csv>
 #notes         :make sure that column heading are stripped on each subsequent run of exiftool in the loop
 #==============================================================================
 
-for day in ${1}/*; do for hour in ${day}/*; do for file in ${hour}/*.jpg; do echo -n ${file}, >> ${2}; rdjpgcom ${file} | sed -n -e '2{p;q}' >> ${2}; done; done; done
+for day in ${1}/*
+    do for hour in ${day}/*
+        do for file in ${hour}/*.jpg
+        do if [ -f ${file} ]
+        then
+            echo -n ${file}, >> ${2}
+            rdjpgcom ${file} | sed -n -e '2{p;q}' >> ${2}
+        fi
+        done
+    done
+done
 
 # this method is actually slower! thought that one file write per loop would be faster...
 #for day in  ${1}/*; do for hour in ${day}/*; do for file in ${hour}/*; do filepath=${file},"; comment=$(rdjpgcom ${file} | sed -n -e '2{p;q}'); echo ${filepath}{$comment} >> ${2}; done; done; done
