12245dd6adc6bb25a94a2326916e2b95d49cdc6f
Jay Goldberg <jaymgoldberg@gmail.com>
2015-04-28 12:45:06 -0500

    add progress bar moved progress printing to commit method add final call to commit actually index now in the commit
    verified a non-uniform entries, count matches input
    616367 /home/jay/CCTVweb/data/apt-south.csv
    
    during insert, then final count:
    > db.aptsouth.find().count()
    70000
    > db.aptsouth.find().count()
    80000
    > db.aptsouth.find().count()
    616367 <--- this matches, yeah!
    
    accept a camera name as argv[1] (maps directory to a mongo collection)

--- backend/scripts/csvimport.py
+++ backend/scripts/csvimport.py
@@ -14,25 +14,23 @@ import pymongo
 from pymongo import MongoClient
 
 class Importer:
-  def __init__(self, numlines_per_chunk):
-    self.chunksize = numlines_per_chunk
+  def __init__(self, numlines_per_chunk, totallines):
+    self.chunksize = numlines_per_chunk # how many lines to read, and insert at a time
     self.iterator = 0
     self.chunk = []
     self.progress = 0
+    self.totallines = totallines
       
   def addline(self, line):
     if self.iterator < self.chunksize:
       self.chunk.append(line)
       self.iterator += 1
     else:
-      # we are missing files at the end
-      print("Commit!")
       self.mongocommit()
       self.chunk.append(line)
       self.iterator += 1
 
   def mongocommit(self):
-    #print('Beginning insert')
     bulk_data = []
     for i in self.chunk:
       pair = i.split(',', 1)
@@ -43,30 +41,38 @@ class Importer:
       bulk_data.append(new_record)
 
     collection.insert(bulk_data)
-    #print('Finished insert')
+    recordsdone = self.progress * self.chunksize
+    print("Committed " + str(recordsdone) + " records. " + "{0:.0f}%".format((float(recordsdone)/self.totallines) * 100))
     self.iterator = 0
     self.chunk = []
     self.progress += 1
-    print(self.progress * self.chunksize)
       
   def complete(self):
+    self.mongocommit()
     collection.create_index("IQimage.time", pymongo.ASCENDING)
 
-filepath = sys.argv[1]
-
-mongochunker = Importer(5000)
+camname = sys.argv[1]
+filepath = sys.argv[2]
 
 if os.path.exists(filepath):
   print('Opening file %s' % filepath)
 
   # TODO: put this in a try block
   f = open(filepath)
+  
+  for lineno, content in enumerate(f):
+    pass
+  
+  f.seek(0)
+  
+  # specify chunksize and total length
+  mongochunker = Importer(10000, lineno+1)
 
   # TODO: define DB settings in config file
   print('Connecting to DB')
-  client = MongoClient()
+  client = MongoClient('mongodb://localhost:27017/')
   db = client.mydb
-  collection = db.images
+  collection = db[camname]
   print('Connected to DB')
   print('Drop collection')
   
@@ -79,6 +85,8 @@ if os.path.exists(filepath):
   f.close()
   
   print("Building index")
+  mongochunker.complete()
+  print("Built index")
   
 else:
   print('File does not exist')
