17814b2597488dff05ad0016c6d659feecda08a6
Jay Goldberg <jaymgoldberg@gmail.com>
2015-04-30 17:30:07 -0500

    Update scraper to consider when rdjpegcom fails. On my bash version on my NAS, it does not support the ${PIPESTATUS} feature of bash, so I fellback to running each command separately. A plus of assigning the output of rdjpgcom to a var is that I don't need sed to cut the lines and can use 'cut'. I feel that this has slowed things down...

--- backend/scripts/scraper.bash
+++ backend/scripts/scraper.bash
@@ -3,22 +3,35 @@
 #title         :scraper.bash
 #description   :This script scans a directory for JPEGs and saves their comment data to a CSV
 #author        :Jay Goldberg
+#url           :https://github.com/JayGoldberg/CCTVweb
 #license       :MIT
 #usage         :bash scraper.bash <dir with day dirs> <filename.csv>
-#notes         :make sure that column heading are stripped on each subsequent run of exiftool in the loop
 #==============================================================================
 
 for day in ${1}/*
-    do for hour in ${day}/*
-        do for file in ${hour}/*.jpg
-        do if [ -f ${file} ]
-        then
-            echo -n ${file}, >> ${2}
-            rdjpgcom ${file} | sed -n -e '2{p;q}' >> ${2}
+  do for hour in ${day}/*
+    do for path in ${hour}/*.jpg
+      do 
+        if [ -f ${path} ]
+          then
+            comment=$(rdjpgcom ${path})
+            if [ $? -eq 0 ]
+              then
+                json=$(cut -d' ' -f2 <<< ${comment})
+                echo ${path},${json} >> ${2}
+            fi
+
         fi
-        done
+      done
     done
-done
+  done
 
 # this method is actually slower! thought that one file write per loop would be faster...
-#for day in  ${1}/*; do for hour in ${day}/*; do for file in ${hour}/*; do filepath=${file},"; comment=$(rdjpgcom ${file} | sed -n -e '2{p;q}'); echo ${filepath}{$comment} >> ${2}; done; done; done
+#for day in  ${1}/*
+#  do for hour in ${day}/*
+#    do for file in ${hour}/*
+#      do
+#        filepath=${file},"; comment=$(rdjpgcom ${file} | sed -n -e '2{p;q}'); echo ${filepath}{$comment} >> ${2}
+#      done
+#    done
+# done
