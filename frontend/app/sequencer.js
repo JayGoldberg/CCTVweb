c6f029070930b62e19f4e694c139e307f72336d8
Jay Goldberg <jaymgoldberg@gmail.com>
2017-03-07 08:01:33 -0800

    Fix bug where images load out of order
    Also beautify

--- frontend/app/sequencer.js
+++ frontend/app/sequencer.js
@@ -28,147 +28,158 @@
  * */
 
 var Sequencer = (function() {
-	var current = -1;
-  var imgList = [];
-	var playInterval;
-  var playDir = 1;
+    var current = -1;
+    var imgList = [];
+    var playInterval;
+    var playDir = 1;
 
-	// configuration defaults
-	var config = {
-		scaleMode : "cover",
-		mouseDirection: "x",
-		playMode: "mouse"
-	};
-  
-  function init(subPlaylist){
-      //subPlaylist.forEach(function(image) {
+    // configuration defaults
+    var config = {
+        scaleMode: "cover",
+        mouseDirection: "x",
+        playMode: "mouse"
+    };
+
+
+    function init(subPlaylist) {
+        //subPlaylist.forEach(function(image) {
         //  console.log('images/' + image);
-      //});
-    var playlist = [];
-    playlist = subPlaylist;
-      configureBody();
-      for (var i = 0, len = playlist.length; i < len; i++) {
-        var img = new Image();
-        console.log(playlist[i])
-        img.src = playlist[i];
-        img.onload = function(){
-          if (imgList.length == 0){
-            canvas.width = this.width;
-            canvas.height = this.height;
-          }
-        imgList.push(this);
-        };
-      }
+        //});
+        var playlist = [];
+        playlist = subPlaylist;
+        configureBody();
+        for (var i = 0, len = playlist.length; i < len; i++) {
+            var img = new Image();
+            console.log(playlist[i])
+            img.src = playlist[i];
+            //http://stackoverflow.com/questions/18936853/read-images-in-loop-wait-for-their-load
+            img.onload = (function(i) {
+                return function() {
+                    if (i == 0) {
+                        canvas.width = this.width;
+                        canvas.height = this.height;
+                    }
+                    imgList[i] = this;
+                }
+            })(i);
+        }
 
-    current = 0;
-    setPlayMode(config.playMode);
-    play();
-	}
-	
-  function setPlayMode(mode){
-		stop();
-		config.playMode = mode;
-	}
-	
-  function play(){
-		stop();
-		if (config.playMode == "mouse"){
-			document.addEventListener('mousemove', onMouseMove, false);
-			document.ontouchmove = function(e){
-				onMouseMove(e.touches[0]);
-				return false;
-			}
-		}
-	}	
 
-  function stop(){
-		document.removeEventListener('mousemove', onMouseMove);
-		if (playInterval) {
-			clearInterval(playInterval);
-			playInterval == null;
-		}
-	}
-			
-  function nextImage(mode){
-		if (!mode) mode = config.playMode;
-    showImage(++current % imgList.length); //loop
-	}
+        current = 0;
+        setPlayMode(config.playMode);
+        play();
+    }
 
-	
-  function onMouseMove(e){
-		var t = imgList.length;
-		var m, w;
-		if (config.mouseDirection == "x") {
-			w = window.innerWidth;
-			m = e.pageX;
-		} else if (config.mouseDirection == "-x") {
-			w = window.innerWidth;
-			m = w - e.pageX - 1;
-		} else if (config.mouseDirection == "y") {
-			w = window.innerHeight;
-			m = e.pageY;
-		} else if (config.mouseDirection == "-y") {
-			w = window.innerHeight;
-			m = w - e.pageY - 1;
-		}
+    function setPlayMode(mode) {
+        stop();
+        config.playMode = mode;
+    }
 
-		var id = Math.min(t, Math.max(0, Math.floor(m / w * t)));
-		if (id != current){
-			showImage(id);
-			current = id;
-		}
-	}
-  
-  function configureBody(){
-		canvas = document.createElement('canvas');
-		canvas.style.position = "fixed";
-		context = canvas.getContext('2d');
-		document.body.appendChild(canvas);
-		document.body.style.height = "100%";
-	}
-	
-  function onWindowResize(){
-		var ca = window.innerWidth / window.innerHeight;
-		var ia = canvas.width / canvas.height;
-		var iw, ih;
-		
-		if (config.scaleMode == "cover") {
-		    if (ca > ia) {
-		    	iw = window.innerWidth;
-		    	ih = iw / ia;
+    function play() {
+        stop();
+        if (config.playMode == "mouse") {
+            document.addEventListener('mousemove', onMouseMove, false);
+            document.ontouchmove = function(e) {
+                onMouseMove(e.touches[0]);
+                return false;
+            }
+        }
+    }
 
-		    } else {
-		    	ih = window.innerHeight;
-		    	iw = ih * ia;
-		    }
-		} else if (config.scaleMode == "contain") {
-		    if (ca < ia) {
-		    	iw = window.innerWidth;
-		    	ih = iw / ia;
-		    } else {
-		    	ih = window.innerHeight;
-		    	iw = ih * ia;
-		    }
-		} else if (config.scaleMode == "auto") {
-		    iw = canvas.width;
-		    ih = canvas.height;
-		}
-			
-		var ox = window.innerWidth/2 - iw/2;
-		var oy = window.innerHeight/2 - ih/2;
-		
-		canvas.style.width = Math.round(iw) + "px";
-		canvas.style.height = Math.round(ih) + "px";
-		canvas.style.left = Math.round(ox) + "px";
-		canvas.style.top = Math.round(oy) + "px";
-	}
-	
-	
-  function showImage(id){
-		if (id >= 0 && id < imgList.length){
-			context.drawImage(imgList[id], 0, 0);
-		}
-	}
-	
-	return  {init : init, nextImage : nextImage, setPlayMode : setPlayMode, play : play, stop : stop};
+    function stop() {
+        document.removeEventListener('mousemove', onMouseMove);
+        if (playInterval) {
+            clearInterval(playInterval);
+            playInterval == null;
+        }
+    }
+
+    function nextImage(mode) {
+        if (!mode) mode = config.playMode;
+        showImage(++current % imgList.length); //loop
+    }
+
+
+    function onMouseMove(e) {
+        var t = imgList.length;
+        var m, w;
+        if (config.mouseDirection == "x") {
+            w = window.innerWidth;
+            m = e.pageX;
+        } else if (config.mouseDirection == "-x") {
+            w = window.innerWidth;
+            m = w - e.pageX - 1;
+        } else if (config.mouseDirection == "y") {
+            w = window.innerHeight;
+            m = e.pageY;
+        } else if (config.mouseDirection == "-y") {
+            w = window.innerHeight;
+            m = w - e.pageY - 1;
+        }
+
+        var id = Math.min(t, Math.max(0, Math.floor(m / w * t)));
+        if (id != current) {
+            showImage(id);
+            current = id;
+        }
+    }
+
+    function configureBody() {
+        canvas = document.createElement('canvas');
+        canvas.style.position = "fixed";
+        context = canvas.getContext('2d');
+        document.body.appendChild(canvas);
+        document.body.style.height = "100%";
+    }
+
+    function onWindowResize() {
+        var ca = window.innerWidth / window.innerHeight;
+        var ia = canvas.width / canvas.height;
+        var iw, ih;
+
+        if (config.scaleMode == "cover") {
+            if (ca > ia) {
+                iw = window.innerWidth;
+                ih = iw / ia;
+
+            } else {
+                ih = window.innerHeight;
+                iw = ih * ia;
+            }
+        } else if (config.scaleMode == "contain") {
+            if (ca < ia) {
+                iw = window.innerWidth;
+                ih = iw / ia;
+            } else {
+                ih = window.innerHeight;
+                iw = ih * ia;
+            }
+        } else if (config.scaleMode == "auto") {
+            iw = canvas.width;
+            ih = canvas.height;
+        }
+
+        var ox = window.innerWidth / 2 - iw / 2;
+        var oy = window.innerHeight / 2 - ih / 2;
+
+        canvas.style.width = Math.round(iw) + "px";
+        canvas.style.height = Math.round(ih) + "px";
+        canvas.style.left = Math.round(ox) + "px";
+        canvas.style.top = Math.round(oy) + "px";
+    }
+
+
+    function showImage(id) {
+        if (id >= 0 && id < imgList.length) {
+            context.drawImage(imgList[id], 0, 0);
+        }
+    }
+
+    return {
+        init: init,
+        nextImage: nextImage,
+        setPlayMode: setPlayMode,
+        play: play,
+        stop: stop
+    };
 })();
