38b23136cd5323d4498fa140fe7d0dff7019fa4c
Jay Goldberg <jaymgoldberg@gmail.com>
2017-03-29 18:16:08 -0700

    per-camera DB now
     - no need for camera name anymore
     - just index `time` field now
     - created a trigger to store deleted in another table rather than using soft-delete column, which was not performant at all
     - more comments
     - will probably create a cam_info table that stores MAC and other facts

--- backend/scripts/sqlite_import.py
+++ backend/scripts/sqlite_import.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 __author__ = "Jay Goldberg"
-__copyright__ = "Copyright 2015"
+__copyright__ = "Copyright 2017"
 __credits__ = ["Jay Goldberg"]
 __license__ = "Apache 2.0"
 __maintainer__ = "Jay Goldberg"
@@ -12,12 +12,12 @@ import sys
 import json
 import sqlite3
 
-def recordgenerator(camname, filepath):
+def recordgenerator(csvfilepath):
 
-  print('Opening file %s' % filepath)
+  print('Opening file %s' % csvfilepath)
 
   # TODO: put this in a try block
-  with open(filepath, 'r') as f:
+  with open(csvfilepath, 'r') as f:
     for lineno, content in enumerate(f):
       pass
   
@@ -31,34 +31,30 @@ def recordgenerator(camname, filepath):
       path = pair[0] # the first element is always the path
       # TODO: error handling for when JSON is not JSON
       decoded = json.loads(pair[1]) # parse the json field in the CSV
-      yield (camname, path, pair[1], decoded['IQimage']['time'], json.dumps(decoded['IQimage']['event']), False)
+      yield (path, pair[1], decoded['IQimage']['time'], json.dumps(decoded['IQimage']['event']))
 
 if __name__ == '__main__':
-  dbname = sys.argv[1]
-  camname = sys.argv[2]
-  filepath = sys.argv[3]
+  dbname = sys.argv[1] # path to .sqlite file, should be camera name
+  csvfilepath = sys.argv[2] # path to csv file with 2 fields, no header
   
   # example JSON: {"IQimage":{"sequence":2542282,"time":1408160269221,"event":["none"],"imgjdbg":" 998/004:16/5/167:080/071/01/4/57/70/20/00:06/06/09:c3"}}
 
   schema = [
       #'cam_id INT',
-      'cam_name TEXT',
       'path TEXT',
       'rawjson TEXT',
       'time INTEGER',
       'event TEXT',
-      'isDeleted INTEGER',
+      #'mac_addr' TEXT,
       #'FOREIGN KEY() REFERENCES cameras(id)',
   ]
 
   conn = sqlite3.connect(dbname)
   
   c = conn.cursor()
-
   schemastring = ''.join(['{}, '.format(field) for field in schema])[:-2]
   
-  c.execute('CREATE TABLE IF NOT EXISTS {}({})'\
-      .format('images', schemastring))
+  c.execute('CREATE TABLE IF NOT EXISTS {}({})'.format('images', schemastring))
 
   # needed when /tmp is too small
   # choose a directory that has enough space for the csv in question
@@ -66,9 +62,13 @@ if __name__ == '__main__':
   c.execute('PRAGMA temp_store = 1')
   c.execute('PRAGMA temp_store_directory = "/tmp"')
 
-  c.executemany('INSERT INTO {}(cam_name, path, rawjson, time, event, isDeleted) VALUES(?, ?, ?, ?, ?, ?)'\
-      .format('images'), recordgenerator(camname, filepath))
+  c.executemany('INSERT INTO {}(path, rawjson, time, event) \
+      VALUES(?, ?, ?, ?)'.format('images'), recordgenerator(csvfilepath))
 
   c.execute('CREATE INDEX IF NOT EXISTS times ON {}(time)'.format('images'))
+  c.execute('CREATE TABLE IF NOT EXISTS {}({})'\
+      .format('image_delete', 'path'))
+  c.execute('CREATE TRIGGER aft_delete AFTER DELETE ON {} BEGIN INSERT into \
+      {}(path) VALUES (OLD.path); END'.format('images', 'image_delete'))
   conn.commit()
   conn.close()
